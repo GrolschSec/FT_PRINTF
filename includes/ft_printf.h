2ab10a1013f20c183f5125e6dbb22262a0375607
Romain <romain@RTFM.local>
2022-07-19 16:02:06 +0200

    Norm

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 17:43:44 by rlouvrie          #+#    #+#             */
-/*   Updated: 2022/07/19 16:01:35 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2022/07/19 16:01:51 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@
 # include <stdarg.h>
 
 int		ft_printf(const char *format, ...);
-void		ft_putchar(char c, int *len);
-void		ft_putstr(char *s, int *len);
-void		ft_putnbr(int n, int *len);
-void		ft_putnbr_unsigned(unsigned int n, int *len);
-void		ft_hexa(unsigned long long int n, int mode, int *len);
-void		ft_address(void *ptr, int *len);
+void	ft_putchar(char c, int *len);
+void	ft_putstr(char *s, int *len);
+void	ft_putnbr(int n, int *len);
+void	ft_putnbr_unsigned(unsigned int n, int *len);
+void	ft_hexa(unsigned long long int n, int mode, int *len);
+void	ft_address(void *ptr, int *len);
 #endif
